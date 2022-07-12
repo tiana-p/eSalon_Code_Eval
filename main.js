da3a3c290a510883c0d0416797076e813ae54d48
tianap <tiana.pehle@gmail.com>
2022-07-12 08:54:37 -0700

    added comments and cleaned up

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 			if($(this).val() == "") {
 				$("span").show();
 			} else {
+            // This will append the information but having some trouble with date and gender
     		$(".submitted-results").append("<p class='display-result-label'>" + $(this).attr("name") + ": </p> <p class='display-result-results'>" + $(this).val() + "</p>"); 
 				 $(".puppy-profile-wrapper").css("display", "none");
 				 $(".submitted-results-container").css("display", "block");
