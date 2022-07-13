812c7e6835e939a93ac27e379494c7e13285cf57
tianap <tiana.pehle@gmail.com>
2022-07-12 18:55:33 -0700

    reformatted js file too

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// Only allows one option to be selected for gender
-	$(".gender-radio:checkbox").on('click', function() {
+	$(".gender-radio:checkbox").on('click', function () {
 		var $genderCheckBox = $(this);
 		if ($genderCheckBox.is(":checked")) {
-		  var group = ".gender-radio:checkbox[name='" + $genderCheckBox.attr("name") + "']";
-		  $(group).prop("checked", false);
-		  $genderCheckBox.prop("checked", true);
+			var group = ".gender-radio:checkbox[name='" + $genderCheckBox.attr("name") + "']";
+			$(group).prop("checked", false);
+			$genderCheckBox.prop("checked", true);
 		} else {
-		  $genderCheckBox.prop("checked", false);
+			$genderCheckBox.prop("checked", false);
 		}
-	  });
+	});
 
-
-	$(".submit-btn").click(function() {
-		$(":input").each(function(index){
-			if($(this).val() == "") {
+	$(".submit-btn").click(function () {
+		$(":input").each(function (index) {
+			if ($(this).val() == "") {
 				$("span").show();
 			} else {
 				// This will append the information but having some trouble with date and gender
 				var submittedName = $("#form-enter-name").val();
-				var submittedDOB = $("#month").val() + "/" + $("#day").val() + "/" + $("#year").val() ;
+				var submittedDOB = $("#month").val() + "/" + $("#day").val() + "/" + $("#year").val();
 				var submittedGender = $('input[name=gender]:checked').val();
 				var submittedMiscNotes = $("#miscNotes").val();
 
@@ -27,13 +26,11 @@ $(document).ready(function() {
 				$('#results-dob').text(submittedDOB);
 				$('#results-gender').text(submittedGender);
 				$('#results-misc').text(submittedMiscNotes);
-				
-				
 
-    		// $(".submitted-results").append("<p class='display-result-label'>" + $(this).attr("name") + ": </p> <p class='display-result-results'>" + $(this).val() + "</p>"); 
-				 $(".puppy-profile-wrapper").css("display", "none");
-				 $(".submitted-results-container").css("display", "block");
+				// $(".submitted-results").append("<p class='display-result-label'>" + $(this).attr("name") + ": </p> <p class='display-result-results'>" + $(this).val() + "</p>"); 
+				$(".puppy-profile-wrapper").css("display", "none");
+				$(".submitted-results-container").css("display", "block");
 			}
-   		});
+		});
 	});
 });
